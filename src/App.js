06e15a035d40b867d40ee8ca65301a2cdfca1f5b
Sumshi <sumayasomow@gmail.com>
2024-02-05 06:50:50 +0300

    completed menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Menu from './pages/Menu';
 
 // for routing pages such as about, home, contact etc use react-router-dom
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -15,6 +16,7 @@ function App() {
         {/*define ur routes inside routes*/}
         {/* <Route path='/' exact component={Home}/> */}
         <Route path='/' exact element={<Home />} />
+        <Route path='/menu' exact element={<Menu />} />
       </Routes>
       <Footer />
      </Router>
